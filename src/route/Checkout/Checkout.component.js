d510de4f2779097873dde8d7420dd2116d4186e2
Alieltahan <alyaltahan@gmail.com>
2022-10-17 17:59:00 +0200

    overwrite Checkout component

---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,40 @@
+import Progressbar from '../Progressbar/Progressbar.component';
+import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component';
+import ContentWrapper from 'SourceComponent/ContentWrapper';
+
+class Checkout extends SourceCheckout {
+  render() {
+    console.log(
+      '🚀 ~ file: Checkout.component.js ~ line 31 ~ Checkout ~ render ~ checkoutStep',
+      this.props.checkoutStep
+    );
+
+    return (
+      <main block="Checkout">
+        <Progressbar
+          stepMap={this.stepMap}
+          checkoutStep={this.props.checkoutStep}
+        />
+        <ContentWrapper
+          wrapperMix={{ block: 'Checkout', elem: 'Wrapper' }}
+          label={__('Checkout page')}
+        >
+          {this.renderSummary(true)}
+          <div block="Checkout" elem="Step">
+            {this.renderTitle()}
+            {this.renderGuestForm()}
+            {this.renderStep()}
+            {this.renderLoader()}
+          </div>
+          <div>
+            {this.renderSummary()}
+            {this.renderPromo()}
+            {this.renderCoupon()}
+          </div>
+        </ContentWrapper>
+      </main>
+    );
+  }
+}
+
+export default Checkout;
